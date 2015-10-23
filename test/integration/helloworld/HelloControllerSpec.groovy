8110f09cac20e72fa92224dfbac1f9fe21351f5b
Alexander Suraphel <asuraphel@apposit.com>
2015-10-23 15:52:03 +0300

    Add an interesting integration test

---
+++ test/integration/helloworld/HelloControllerSpec.groovy
@@ -0,0 +1,23 @@
+package helloworld.integration
+
+
+
+import spock.lang.*
+
+/**
+ *
+ */
+class HelloControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+
+        expect:
+            3 == 3
+    }
+}
